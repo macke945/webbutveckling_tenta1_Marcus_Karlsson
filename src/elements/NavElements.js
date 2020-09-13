267c2bef39e23c5b0700a487856bbec4c5e54bd0
macke945 <55479431+macke945@users.noreply.github.com>
2020-09-13 02:17:15 +0200

    added footer

---
+++ src/elements/NavElements.js
@@ -0,0 +1,17 @@
+import styled from "styled-components"
+
+//sets at what grid the nav should be
+export const NavWrapper = styled.nav`
+    grid-column: 2 / span 12;
+    grid-row: 1/ 2;
+    display: flex;
+    align-items: center;
+
+    & img {
+        height: 30px;
+    }
+
+    @media ${props => props.theme.breakpoints.tablet} {
+        grid-column: 2 / span 6;
+    }
+`
